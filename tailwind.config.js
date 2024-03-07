aa7fb3aba03174870c5f33e4e35cc4e4c2abfb5c
Jeweljeff <ketachi.jeff@gmial.com>
2024-03-07 01:43:08 +0100

    first 3 pages done.

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    screens: {
+      sm: "376px",
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      colors: {
+        GreenT: '#008000',
+        GreenL: '#00B800',
+        Graytrue: '#141414',
+        Gray: '#475467',
+        Orange: '#D45800',
+        GreenB: '#556B2F',
+        TextGray: '#737373'
+
+      },
+      fontFamily: {
+        Work: ['Work Sans'],
+        Man: ['Manrope']
+      }
+    },
+  },
+  plugins: [],
+}
+
