2d2adc6d8f7b0e415f0c221f50a7429ade9a4a39
Queen-jewel <ketachi.jeff@gmail.com>
2024-05-21 21:13:22 +0100

    customerview page complete

--- JS/custom.js
+++ JS/custom.js
@@ -1,13 +1,12 @@
+// page change
 const flip = document.getElementById('Switch');
-
-
 
 flip.addEventListener('click', () => {
     window.location.href = '/ownerview.html';
 })
 const divs = document.querySelectorAll('.greenB');
 
-
+// product border change
 divs.forEach(div => {
 div.addEventListener('click', () => {
     if (div.style.border === '1px solid transparent') {
@@ -16,4 +15,48 @@ div.addEventListener('click', () => {
         div.style.border = '1px solid transparent';
       }
     });
+});
+
+// Dropdown toggle
+const downC = document.querySelector('#dropdown');
+const toggleDropdown = () => {
+  downC.classList.toggle('hidden');
+}
+
+
+// Image change
+const image = document.getElementById('image');
+
+image.addEventListener('click', ()=> {
+  if(image.src.match("DNC")) {
+    image.src = '/images/UPC.svg';
+  } else {
+    image.src = '/images/DNC.svg';
+  }
+})
+
+// categories change color on click
+const cats = document.querySelectorAll('.click');
+const cate = document.getElementById('cate');
+const cat1 = document.getElementById('cat1');
+const cat2 = document.getElementById('cat2');
+const cat3 = document.getElementById('cat3');
+
+cats.forEach(cat => {
+  cat.addEventListener('click', ()=> {
+    if(cat.style.fontWeight = 'normal'){
+    cat.style.fontWeight = 'bold';
+    cat.style.color = '#00B800';
+  }
+  });
+});
+
+cat1.addEventListener('click', ()=> {
+  cate.textContent = 'category 1'
+});
+cat2.addEventListener('click', ()=> {
+  cate.textContent = 'category 2'
+});
+cat3.addEventListener('click', ()=> {
+  cate.textContent = 'category 3'
 });
