be1c3cddf96f878a6fdd20c9ca0ec23d503d5dec
Queen-jewel <ketachi.jeff@gmail.com>
2024-05-28 20:08:22 +0100

    owners page complete

--- JS/owner.js
+++ JS/owner.js
@@ -5,15 +5,3 @@ const flip = document.getElementById('Switch');
 flip.addEventListener('click', () => {
     window.location.href = '/customerview.html';
 })
-const divs = document.querySelectorAll('.greenB');
-
-
-divs.forEach(div => {
-div.addEventListener('click', () => {
-    if (div.style.border === '1px solid transparent') {
-        div.style.border = ' 3px solid green';
-      } else {
-        div.style.border = '1px solid transparent';
-      }
-    });
-});
