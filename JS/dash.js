38733d7dbb2e6c2cf7f3a07ee40d4bea4db5e812
Queen-jewel <ketachi.jeff@gmail.com>
2024-06-15 01:13:48 +0100

    first dashboard

--- JS/dash.js
+++ JS/dash.js
@@ -1,24 +1,34 @@
 document.addEventListener('DOMContentLoaded',()=> {
-const line = document.getElementById('lineChart').getContext('2d');
+const ctx = document.getElementById('lineChart').getContext('2d');
 
 // Define gradient fill (vertical from top to bottom)
-var gradient = line.createLinearGradient(0, 0, 10, 400);
+var gradient = ctx.createLinearGradient(0, 0, 10, 400);
 gradient.addColorStop(0, 'rgba(0, 128, 0, 1)');
 gradient.addColorStop(1, 'rgba(0, 184, 0, 1)');
 
-let chart = new Chart(line, {
+new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['12am', '3am', '9am', '12pm', '3pm', '9pm', '11:59am'],
+        labels: ['12am', '3am','6am', '9am', '12pm', '3pm', '6pm', '9pm', '11:59am'],
         datasets:[{
             label:'',
-            data:['0k','50k', '100k', '150k', '200k', '250+'],
+            data:[0, 0, 0, 50, 120, 70, 50 , 150, 100, 200, 250],
             backgroundColor: gradient,
             borderColor: 'rgba(0, 128, 0, 1)',
             fill: false,
-            tension:0.5
+            tension:0.4
         }]
     },
+    options: {
+        scales: {
+          x: {
+            grid: {
+              display: false
+            },
+            
+          },
+        }
+      }
 });
 
 // Doughnut Chart
@@ -49,3 +59,37 @@ let chart = new Chart(line, {
         }
     })
 });
+
+// Bar Chart
+
+const bar = document.getElementById('barchart').getContext('2d');
+// Define gradient fill (vertical from top to bottom)
+const gradient = bar.createLinearGradient(0, 0, 10, 400);
+gradient.addColorStop(0, 'rgba(0, 128, 0, 1)');
+gradient.addColorStop(1, 'rgba(0, 184, 0, 1)');
+
+new Chart(bar, {
+    type:'bar',
+    data: {
+        labels: ['12am-3am', '3;01am-6am', '6:01am-9am', '9:01am-12pm', '12:01pm-3pm', '3:01pm-6pm', '6:01pm-9pm', '9:01pm-11:59pm'],
+        datasets: [{
+            data:[2, 40, 80, 90, 20, 45, 100, 110, 120],
+            backgroundColor: gradient,
+            barThickness: 80, // increase the thickness of the bars
+            categorySpacing: 60, 
+            label:'',
+        }]
+    },
+    options: {
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+        },
+        dataset: {
+            label: false, // remove dataset label
+          }
+      }
+})
